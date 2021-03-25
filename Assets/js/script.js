37a072d81b3807d4355fe1d62edd7d7b689cf15f
Alejandra Gutierrez <gutihi85@gmail.com>
2021-03-25 12:41:03 -0600

    adding some conditions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,21 +4,34 @@ var generateBtn = document.querySelector("#generate");
 // add an event listener
 generateBtn.addEventListener("click", function() {
   
-  // -- prompt for length
+  // prompt for length
   var length = prompt('Enter desired length between 8-128 characters.')
-  // -- prompt for lowercase
+  // prompt for lowercase
   var wantLowercase = confirm('Do you want lowercase letters?');
-  // -- prompt for uppercase
+  // prompt for uppercase
   var wantUppercase = confirm('Do you want uppercase letters?');
-  // -- prompt for numbers
+  // prompt for numbers
   var wantNumbers = confirm('Do you want numbers?');
-  // -- prompt for special char
+  // prompt for special char
   var wantSpecialCharacters = confirm('Do you want special characters?');
   
+  // stablish what is lowerCase
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz ".split("");
+  // stablish what is upperCase
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ".split("");
+  // stablish what is numbers
+  var numbers= "0123456789 ".split("");
+  // stablish what is specialCharacters
+  var lowerCase = "!@#$%&/?¿ ".split("");
   
   // use that prompt info to calculate a password
   // in the mean time
   var password = 'This is your passsword shhhh'
+
+  // condition what goes into password
+  // if (wantLowercase===true && wantUppercase===true && wantNumbers===true && wantSpecialCharacters===true){
+  //   password = ""
+  // }
   
   // ---display the password to the user---
   var passwordText = document.querySelector("#password");
